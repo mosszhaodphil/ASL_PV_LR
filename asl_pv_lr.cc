26e63d8ee5c7b7b1f2fba719622e4f31bedd519b
mosszhaodphil <mosszhao@Mosss-MacBook-Pro.local>
2015-05-18 09:35:57 +0100

    removed pvwm pvgm options
    Now all work on single PV map

--- asl_pv_lr.cc
+++ asl_pv_lr.cc
@@ -43,20 +43,14 @@ int main(int argc, char *argv[])
     // load data (3D matrix)
     volume<float> data;
     read_volume(data, opts.datafile.value());
-    //volume4D<float> data;
-    //read_volume4D(data,opts.datafile.value());
 
     // load mask
     volume<float> mask;
     read_volume(mask, opts.maskfile.value());
 
     // load pvgm
-    volume<float> pvgm;
-    read_volume(pvgm, opts.pvgmfile.value());
-
-    // load pvwm
-    volume<float> pvwm;
-    read_volume(pvwm, opts.pvwmfile.value());
+    volume<float> pv_map;
+    read_volume(pv_map, opts.pvfile.value());
 
     // load output file name
     string outname;
@@ -66,147 +60,12 @@ int main(int argc, char *argv[])
     int kernel;
     kernel = opts.kernel.value();
 
-    // GM and WM corrected single TI file
-    string gm_out_filename;
-    gm_out_filename = gm_out_filename + data_filename + "_gm";
+    // Empty 3D matrix to save PV corrected results
+    volume<float> data_pv_corr;
 
-    string wm_out_filename;
-    wm_out_filename = wm_out_filename + data_filename + "_wm";
+    pv_correct(data, mask, pv_map, kernel, data_pv_corr);
 
-    // Empty 3D matrix to save  PV corrected results
-    volume<float> data_gm;
-    volume<float> data_wm;
-
-    pv_correct(data, mask, pvgm, kernel, data_gm);
-    //pv_correct(data, mask, pvwm, kernel, data_wm);
-
-    save_volume(data_gm, gm_out_filename);
-    //save_volume(data_wm, wm_out_filename);
-
-    /*
-
-
-    // load mask
-    volume<float> mask(data.xsize(),data.ysize(),data.zsize());
-    mask.setdims(data.xdim(),data.ydim(),data.zdim());
-    read_volume(mask,opts.maskfile.value());
-
-    Matrix asldata;
-    asldata = data.matrix(mask); // creates a 2D matrix of all voxels and their time series
-    //data.setmatrix(asldata,mask);
-    int nvox=asldata.Ncols();
-    
-    //load aif
-    volume4D<float> aif;
-    read_volume4D(aif,opts.aif.value());
-
-    
-    // select AIF based on metric
-    // load metric image if it exists
-    volume<float> metric;
-    if (opts.metric.set()) {
-      cout << "Preparing AIFs" << endl;
-      read_volume(metric,opts.metric.value());
-
-      Prepare_AIF(aif, metric, mask, opts.mthresh.value());
-      
-      //volume4D<float> aifout;
-      //aifout.setmatrix(aif,mask);
-      save_volume4D(aif,opts.outname.value()+"_aifs");
-    }
-
-    Matrix aifmtx;
-    aifmtx = aif.matrix(mask);
-
-    // do deconvolution
-    cout << "Performing deconvolution" << endl;
-    ColumnVector mag; // Column Vector to save CBF of each voxel (taken as the largest value of each column of scaled residue matrix)
-    Matrix resid;
-    Deconv(asldata,aifmtx,opts.dt.value(),mag,resid);
-
-    // estimate BAT (of tissue)
-    ColumnVector batt;
-    if (opts.batout.set() | (opts.tcorrect.set() & !opts.batt.set())) {
-      cout << "Estimating BAT" << endl;
-      Estimate_onset(asldata,batt,opts.dt.value());
-
-      if (opts.batout.set()) {
-	//output the BAT image (from the tissue)
-        volume4D<float> batout;
-        batout.setmatrix(batt.AsMatrix(1,nvox),mask);
-        save_volume4D(batout,opts.outname.value()+"_bat");
-      }
-    }
-
-    // correct aif magntiude for differences in arrival time between aif and tissue 
-    ColumnVector batd;
-    if(opts.tcorrect.set()) {
-      cout << "Performing timing correction" << endl;
-      
-
-      if (opts.bata.set()) {
-	//calculate difference between tissue and AIF curves using suppled BAT images
-        volume4D<float> bat_art;
-        read_volume4D(bat_art,opts.bata.value());
-        if (opts.batt.set()) {
-	  //load supplied tissue BAT
-          volume4D<float> bat_tiss;
-          read_volume4D(bat_tiss,opts.batt.value());
-          batt = (bat_tiss.matrix(mask)).AsColumn();
-        }
-
-        if (opts.metric.set()) {
-	  // correct the AIF bat image to match the AIFs where a metric image has been supplied
-          Prepare_AIF(bat_art,metric,mask,opts.mthresh.value());
-        }
-        ColumnVector bata;
-        bata = (bat_art.matrix(mask)).AsColumn();
-        batd = batt-bata;
-      }
-      else {
-	//otherwise estimate BAT difference using the peak in the residue function
-	//Estimate_BAT_difference(resid,batd,opts.dt.value());
-
-	// Estiamte BAT difference using edge detection
-        ColumnVector bata;
-        Estimate_onset(aifmtx,bata,opts.dt.value());
-        batd = batt-bata;
-      }
-
-      for (int i=1; i<=batd.Nrows(); i++) {
-        if (batd(i)<0.0)
-          batd(i)=0.0;
-      }
-      Correct_magnitude(mag,batd,opts.T1.value(),opts.dt.value(),opts.fa.value());
-    }
-
-    if(opts.std.set()) {
-      // do wild boostrapping std dev estimation for cbf
-      cout << "Performing wild bootstrapping for precision estimation" << endl;
-      ColumnVector magstd;
-      BootStrap(aifmtx, asldata, opts.dt.value(), mag, resid, opts.nwb.value(), magstd);
-      if (opts.tcorrect.set()) {
-	// if needed we should correct the std dev for timing discrpancies between aif and ctc
-        Correct_magnitude(magstd,batd,opts.T1.value(),opts.dt.value(),opts.fa.value());
-      }
-
-      // save it
-      volume4D<float> stdoutVol; //stdout is a reserved name - can cause weird compile errors
-      stdoutVol.setmatrix(magstd.AsMatrix(1,nvox),mask);
-      save_volume4D(stdoutVol,opts.outname.value()+"_stddev");
-    }
-    
-    cout << "Saving results" << endl;
-    //output 
-    volume4D<float> residout;
-    residout.setmatrix(resid,mask);
-    save_volume4D(residout,opts.outname.value()+"_residuals");
-
-    volume4D<float> magout;
-    magout.setmatrix(mag.AsMatrix(1,nvox),mask);
-    save_volume4D(magout,opts.outname.value()+"_magntiude");
-
-    */
+    save_volume(data_pv_corr, outname);
 
     cout << "ASL_PV_LR - Done!" << endl;
 
--- readoptions.h
+++ readoptions.h
@@ -28,26 +28,9 @@ public:
 
   Option<string> datafile;
   Option<string> maskfile;
-  Option<string> pvgmfile;
-  Option<string> pvwmfile;
+  Option<string> pvfile;
   Option<string> outname;
-  //Option<string> aif;
   Option<int> kernel;
-
-  //Option<string> metric;
-  //Option<float> mthresh;
-
-  // timing correction
-  //Option<bool> tcorrect;
-  //Option<string> bata;
-  //Option<string> batt;
-  //Option<bool> batout;
-  //Option<float> T1;
-  //Option<float> fa;
-
-  // std deviation estimation
-  //Option<bool> std;
-  //Option<int> nwb;
 
   void parse_command_line(int argc, char** argv);
 
@@ -83,13 +66,6 @@ help(string("-h,--help"), false,
   string("display this message"),
   false, no_argument),
 
-//input files
-/*
-datafile(string("--data,--datafile"), string("data"),
-  string("ASL data file"),
-  true, requires_argument),  
-*/
-
 // Compulsory arguments
 datafile(string("--data,--datafile"), string("data"),
   string("Single_TI data file"),
@@ -98,11 +74,8 @@ datafile(string("--data,--datafile"), string("data"),
 maskfile(string("--mask"), string("mask"),
   string("mask"), true, requires_argument),
 
-pvgmfile(string("--pvgm"), string("pvgm"),
+pvfile(string("--pv"), string("pvmap"),
   string("GM PV map"), true, requires_argument),
-
-pvwmfile(string("--pvwm"), string("pvwm"),
-  string("WM PV map"), true, requires_argument),
 
 outname(string("--out"), string("outname"),
   string("Output directory name"), true, requires_argument),
@@ -110,86 +83,25 @@ outname(string("--out"), string("outname"),
 kernel(string("--kernel"), 5,
   string("Kernel size (Default: 5)"), true, requires_argument),
 
-/*
-outname(string("--out"), string("asl_mfree"),
-  string("Output directory name"), true, requires_argument),
-*/
-
-/*
-aif(string("--aif"),string(""),
-  string("Arterial input functions for deconvolution (4D volume, one aif for each voxel within mask)"),
-  true,requires_argument),
-
-dt(string("--dt"),1.0,
-  string("Temporal spacing in data (s)\n"),
-  true,requires_argument),
-
-metric(string("--metric"),string(""),
-  string("Metric image"), false,requires_argument),
-
-mthresh(string("--mthresh"),0.0,
-  string("Metric threshold\n"), false,requires_argument),
-
-tcorrect(string("--tcorrect"),false,
-  string("Apply correction for timing difference between AIF and tissue curve"),
-  false,no_argument),
-
-bata(string("--bata"),string(""), string("arterial BAT image"), false,requires_argument),
-
-batt(string("--batt"),string(""), string("tissue BAT image"), false,requires_argument),
-
-batout(string("--bat"),false, 
-  string("Estimate tissue BAT from data (and save this image)"), false,no_argument),
-
-T1(string("--t1"),1.6, string("T1 (of blood) value"), false,requires_argument),
-
-fa(string("--fa"),0.0, string("Flip anlge (is using LL readout)"), false,requires_argument),
-
-std(string("--std"),false,
-  string("Calculate standard deviations on perfusion values using wild bootstrapping"),
-  false,no_argument),
-
-nwb(string("--nwb"),1000,
-  string("Number of permuatations for wild bootstrapping"),
-  false,requires_argument),
-*/
-//options("asl_mfree","asl_mfree --verbose\n") {
 options("asl_pv_lr","asl_pv_lr --verbose\n") {
   try {
     options.add(help);
 
     options.add(datafile);
     options.add(maskfile);
-    options.add(pvgmfile);
-    options.add(pvwmfile);
+    options.add(pvfile);
     options.add(outname);
     options.add(kernel);
-    //options.add(aif);
-    //options.add(dt);
 
-       //options.add(metric);
-       //options.add(mthresh);
-
-       //options.add(tcorrect);
-       //options.add(bata);
-       //options.add(batt);
-       //options.add(batout);
-       //options.add(T1);
-       //options.add(fa);
-       
-       //options.add(std);
-       //options.add(nwb);
-
-        }
-     catch(X_OptionError& e) {
-       options.usage();
-       cerr << endl << e.what() << endl;
-     } 
-     catch(std::exception &e) {
-       cerr << e.what() << endl;
-     }    
-     
-   }
+  }
+  catch(X_OptionError& e) {
+    options.usage();
+    cerr << endl << e.what() << endl;
+  }
+  catch(std::exception &e) {
+    cerr << e.what() << endl;
+  }
+}
 
 }
 #endif
